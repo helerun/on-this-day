c90d7581d50c2cd3e15f59baf680fefe36cf3c9f
helerun <liyrauhn@gmail.com>
2024-03-07 16:46:53 +0100

    versione da finire

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,21 +8,24 @@ function Card(props) {
       //aprimi la card
     }
   }
+
   return (
     <div className="container" onClick={openClose}>
       <div className="cardTop">
-        <span className="cardEvent">{props.place}</span>
-        <span className="cardYear">{props.year}</span>
+        <span className="cardEvent">Luogo</span>
+        <span className="cardYear">2000</span>
       </div>
       <div className="cardDescription">
         <div className="cardLeft">
           <img
-            src={props.img}
+            src="https://media.istockphoto.com/id/1147544807/it/vettoriale/la-commissione-per-la-immagine-di-anteprima-grafica-vettoriale.jpg?s=612x612&w=0&k=20&c=gsxHNYV71DzPuhyg-btvo-QhhTwWY0z4SGCSe44rvg4="
             className="cardImg"
           />
         </div>
         <div className="cardRight">
-          <p className="cardText">{props.text}</p>
+          <p className="cardText">
+            Bradman plays his last innings in 1st-class cricket, gets 30!
+          </p>
         </div>
       </div>
     </div>
